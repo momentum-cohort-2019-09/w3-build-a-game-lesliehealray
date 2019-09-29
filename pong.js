d225ee957087be947a34aefe34d2fbd38fd0ee98
lesliehealray <lesliehealray@gmail.com>
2019-09-28 23:14:30 -0400

    adding moving player up down left right

--- pong.js
+++ pong.js
@@ -6,6 +6,8 @@ class Game {
         this.leftPressed = false;
         this.gameOver = false;
         this.bodies = [];
+        this.elapsedTime = 0;
+        this.keyboard = new Keyboarder()
 
         this.player = new Player(this)
         this.addBody(this.player)
@@ -19,6 +21,11 @@ class Game {
     }
 
     update (){
+        this.elapsedTime += 1
+        if (this.elapsedTime % 120 === 0){
+            let ball = new Ball(this)
+            this.addBody(ball)
+        }
         for (let body of this.bodies) {
             body.update(this)
         }
@@ -51,9 +58,13 @@ class Ball{
     constructor(game) {
         this.ballRadius = 10;
         this.x = game.canvas.width/2;
-        this.y = game.canvas.height-30;
-        this.xVelocity = 2;
-        this.yVelocity = -2;
+        this.y = game.canvas.height/2;
+        let x_rand = 1 + Math.floor(Math.random() * Math.floor(3));
+        x_rand *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        this.xVelocity = x_rand;
+        let y_rand = 1 + Math.floor(Math.random() * Math.floor(3));
+        y_rand *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        this.yVelocity = y_rand;
     }
     draw() {
         game.screen.beginPath();
@@ -63,28 +74,83 @@ class Ball{
         game.screen.closePath();
     }
     update(){
-        console.log("updated")
+        if(this.x + this.xVelocity > game.canvas.width-this.ballRadius || this.x + this.xVelocity < this.ballRadius) {
+            this.xVelocity = -this.xVelocity;
+        }
+       
+        else if (this.y + this.yVelocity > game.canvas.height - this.ballRadius || this.y + this.yVelocity< this.ballRadius){
+            this.yVelocity = -this.yVelocity;
+        }
+        // else if(this.y + this.yVelocity > game.canvas.height-this.ballRadius) {
+        //     // if(this.x > game.paddleX && this.x < game.paddleX + game.paddleWidth) {
+        //         this.yVelocity = -this.yVelocity;
+        //     // }
+        // }
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
     }
 }
 
 class Player{
     constructor(game) {
-        this.paddleHeight = 10;
-        this.paddleWidth = 75;
+        this.paddleHeight = 20;
+        this.paddleWidth = 20;
         this.paddleX = (game.canvas.width-this.paddleWidth)/2;
+        this.paddleY = (game.canvas.height)
     }
     draw() {
         game.screen.beginPath();
-        game.screen.rect(this.paddleX, game.canvas.height-this.paddleHeight, this.paddleWidth, this.paddleHeight);
+        game.screen.rect(this.paddleX, this.paddleY, this.paddleWidth, this.paddleHeight);
         game.screen.fillStyle = "black";
         game.screen.fill();
         game.screen.closePath();
     }
 
     update(){
-        console.log("updated")
-    }
+        if (game.keyboard.isDown(Keyboarder.KEYS.LEFT) && this.paddleX > 0) {
+            this.paddleX -= 7;
+        } else if (game.keyboard.isDown(Keyboarder.KEYS.RIGHT) && this.paddleX < game.canvas.width-this.paddleWidth){
+            this.paddleX += 7;
+        } else if (game.keyboard.isDown(Keyboarder.KEYS.UP) && this.paddleY < game.canvas.height-this.paddleHeight){
+            this.paddleY +=7;
+            console.log('UP')
+        } else if (game.keyboard.isDown(Keyboarder.KEYS.DOWN) && this.paddleY > 0){
+            this.paddleY -= 7;
+            console.log('DOWN')
+
+        }
+    }   
 }
+
+  
+class Keyboarder {
+    constructor () {
+      this.keyState = {}
+  
+      document.addEventListener('keydown', function (e) {
+        this.keyState[e.keyCode] = true
+      }.bind(this))
+  
+      document.addEventListener('keyup', function (e) {
+        this.keyState[e.keyCode] = false
+      }.bind(this))
+    }
+  
+    isDown (keyCode) {
+      return this.keyState[keyCode] === true
+    }
+  
+    on (keyCode, callback) {
+      document.addEventListener('keydown', function (e) {
+        if (e.keyCode === keyCode) {
+          callback()
+        }
+      })
+    }
+  }
+  
+  Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 }
+
 
 const game = new Game('pong')
 game.run()
